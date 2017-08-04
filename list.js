c4c10e8513cdc436c9bccfb6348bed5e044c68da
EricRiney <fails.to.install@gmail.com>
2017-08-04 16:27:03 -0700

    need a txt converter

--- list.js
+++ list.js
@@ -1,3 +1,6 @@
+// abstract data type (ADT) list implementation. 
+// kinda... I think.
+
 function List() {
    this.listSize = 0;
    this.pos = 0;
@@ -19,10 +22,15 @@ function List() {
    this.length = length;
 }
 
+// The append() function appends a new element onto the list 
+// at the next available position, which will be equal to 
+// the value of the listSize variable:
 function append(element) {
    this.dataStore[this.listSize++] = element;
 }
 
+// remove() is a helper function and one of the harder 
+// functions to implement in the List class
 function find(element) {
    for (var i = 0; i < this.dataStore.length; ++i) {
       if (this.dataStore[i] == element) {
@@ -32,6 +40,10 @@ function find(element) {
    return -1;
 }
 
+// remove() function uses the position returned by find() 
+// to splice the dataStore array at that place. 
+// After the array is modified, listSize is decremented 
+// by 1 to reflect the new size of the list.
 function remove(element) {
    var foundAt = this.find(element);
    if (foundAt > -1) {
@@ -42,10 +54,12 @@ function remove(element) {
    return false;
 }
 
+// ...
 function toString() {
     return this.dataStore;
 }
 
+// Clears the list
 function clear() {
     delete this.dataStore; 
     this.dataStore = [];    
@@ -53,6 +67,9 @@ function clear() {
     this.pos = 0;
 }
 
+// An insertion function needs to know where to insert an element, 
+// so for now we will say that insertion occurs after 
+// a specified element already in the list.
 function insert(element, after) { 
     var insertPos = this.find(after); 
     if (insertPos > -1) {
@@ -63,6 +80,8 @@ function insert(element, after) {
     return false; 
 }
 
+// contains() function is useful when you want to check a 
+// list to see if a particular value is part of the lis
 function contains(element) {
     for (var i = 0; i < this.dataStore.length; ++i) {
         if (this.dataStore[i] == element) { 
@@ -72,6 +91,7 @@ function contains(element) {
     return false; 
 }
 
+// The length() function returns the number of elements in a list
 function length() { 
     return this.listSize;
 }
@@ -109,17 +129,17 @@ function getElement() {
     return this.dataStore[this.pos];
 }
 
+// var names = new List();
+// names.append("Cynthia");
+// names.append("Raymond");
+// names.append("Barbara");
+// console.log(names.toString());
+// names.remove("Raymond");
+// console.log(names.toString());
 
-var names = new List();
-names.append("Cynthia");
-names.append("Raymond");
-names.append("Barbara");
-console.log(names.toString());
-names.remove("Raymond");
-console.log(names.toString());
-
-// var movies = createArr("movies.txt");
-// var movieList = new List();
+var movies = createArr("movies.txt");
+var movieList = new List();
+console.log(movies);
 // var customers = new List();
 // for (var i = 0; i < movies.length; ++i) {
 //    movieList.append(movies[i]);
