e8fea4f1edcd635fa630f6d9db290fa16b8712ab
EricRiney <fails.to.install@gmail.com>
2017-08-03 18:41:27 -0700

    movies.txt added

--- list.js
+++ list.js
@@ -53,3 +53,15 @@ names.append("Barbara");
 print(names.toString());
 names.remove("Raymond");
 print(names.toString());
+
+var movies = createArr("films.txt");
+var movieList = new List();
+var customers = new List();
+for (var i = 0; i < movies.length; ++i) {
+   movieList.append(movies[i]);
+}
+print("Available movies: \n");
+displayList(movieList);
+checkOut("Jane Doe", "The Godfather", movieList, customers);
+print("\nCustomer Rentals: \n");
+displayList(customers);
