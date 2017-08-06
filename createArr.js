9271cceda6acedef2c6dab336cf7e2e13e805987
EricRiney <fails.to.install@gmail.com>
2017-08-05 19:28:42 -0700

    constructor added to stack

--- createArr.js
+++
@@ -1,6 +0,0 @@
-function createArr(file) {
-var arr = read(file).split("\n");
-for (var i = 0; i < arr.length; ++i) {
-          arr[i] = arr[i].trim();
-       }
-return arr; }
---
+++ stack.js
@@ -0,0 +1,9 @@
+// Gonna build a damn stack using an array.
+
+function Stack() { 
+    this.dataStore = []; 
+    this.top = 0; 
+    this.push = push; 
+    this.pop = pop; 
+    this.peek = peek;
+}
