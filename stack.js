0aeeab0d2fd066ccb7e76d438bcb4e5069827829
EricRiney <fails.to.install@gmail.com>
2017-08-07 08:51:53 -0700

    stack fin

--- stack.js
+++ stack.js
@@ -6,4 +6,35 @@ function Stack() {
     this.push = push; 
     this.pop = pop; 
     this.peek = peek;
+    this.clear = clear;
+    this.length = length;
 }
+
+// When we push a new element onto a stack, 
+// we have to store it in the top position and 
+// increment the top variable so that the new top 
+// is the next empty position in the array. 
+// Here is the code:
+function push(element) { 
+    this.dataStore[this.top++] = element;
+}
+
+// The pop() function does the reverse of the push() function—
+// it returns the element in the top position of 
+// the stack and then decrements the top variable:
+function pop() {
+    return this.dataStore[--this.top];
+}
+
+function peek() {
+    return this.dataStore[this.top-1];
+}
+
+function length() { 
+    return this.top;
+}
+
+function clear() {
+    this.top = 0;
+}
+
